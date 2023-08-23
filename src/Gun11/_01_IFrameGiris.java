14823618f7a53ca40b8b5675fa38b9d36119ee96
TechnoStudy <ismettechno@gmail.com>
2023-08-23 18:05:28 +0100

    Selenium Gun11 Frame Giriş fix

--- src/Gun11/_01_IFrameGiris.java
+++ src/Gun11/_01_IFrameGiris.java
@@ -11,13 +11,17 @@ public class _01_IFrameGiris extends BaseDriver {
     public void Test1()
     {
         driver.get("https://chercher.tech/practice/frames");
+        long startTime = System.currentTimeMillis();
 
-        //driver.switchTo().frame(0);  // index vererek geçilebilir : index sayfadaki sırası
-        //driver.switchTo().frame("frame1"); // id vererek geçebilirsiniz, ancak yavaş
-        //driver.switchTo().frame("iamframe"); // name vererek de geçilebilir, ancak hızlı
+        //driver.switchTo().frame(0);  //24 index vererek geçilebilir : index sayfadaki sırası
+        //driver.switchTo().frame("frame1"); //20112 id vererek geçebilirsiniz, ancak yavaş
+        //driver.switchTo().frame("iamframe"); //163 name vererek de geçilebilir, ancak hızlı
         WebElement frame1=driver.findElement(By.id("frame1"));
-        driver.switchTo().frame(frame1); // weblement olarakda verebilirsin, ancak hızlı
-        // index>name>webelement>id
+        driver.switchTo().frame(frame1); // 71 weblement olarakda verebilirsin, ancak hızlı
+        // index>webelement>name>id   selenium un versiyonuna göre değişir
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Süre = " + (endTime-startTime));
 
         WebElement txtBox=driver.findElement(By.cssSelector("[id='topic']+input"));
         txtBox.sendKeys("Türkiye");
