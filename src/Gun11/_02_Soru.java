d4738043c347a58555621cb61c2f57ccd95f4d03
TechnoStudy <ismettechno@gmail.com>
2023-08-23 18:09:14 +0100

    Selenium Gun11 Frame Soru

---
+++ src/Gun11/_02_Soru.java
@@ -0,0 +1,14 @@
+package Gun11;
+
+
+/*
+   Senaryo :
+   https://chercher.tech/practice/frames sitesine gidiniz
+   Ekranda gözüken ilk kutucuğa Türkiye yazınız.
+   Daha sonra animals dan seçeneklerinden avatar ı seçiniz.
+ */
+
+
+
+public class _02_Soru {
+}
