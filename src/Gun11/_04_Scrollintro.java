8f6f3a70af91d1f766470ebc1f2f049bd3eaaeed
TechnoStudy <ismettechno@gmail.com>
2023-08-23 19:43:08 +0100

    Selenium Gun11 JavaScript : Scroll 1 fix

--- src/Gun11/_04_Scrollintro.java
+++ src/Gun11/_04_Scrollintro.java
@@ -21,8 +21,10 @@ public class _04_Scrollintro extends BaseDriver {
         js.executeScript("window.scrollTo(0, 500);"); // x, y  : baştan verilen yere gider
 
         MyFunc.Bekle(2);
-
         js.executeScript("window.scrollBy(0, 500);"); // en kaldığı yerden 500 daha gider
+
+        MyFunc.Bekle(2);
+        js.executeScript("window.scrollBy(0, -200);"); // en kaldığı yerden 200 geri gider
 
         BekleVeKapat();
     }
