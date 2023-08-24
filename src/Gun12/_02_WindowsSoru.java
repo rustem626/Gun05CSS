ae86523e8fa839b4ff9071b9adb731026303e190
TechnoStudy <ismettechno@gmail.com>
2023-08-24 18:16:39 +0100

    Selenium Gun11 WindowsHandle Soru

---
+++ src/Gun12/_02_WindowsSoru.java
@@ -0,0 +1,11 @@
+package Gun12;
+
+/*
+    Senaryo:
+    https://www.selenium.dev/ adresindeki ayrı tab da açılan linklere tıklatınız.
+    Açılan bütün yeni tablardaki sayfaların title ve url lerini yazdırınız.
+    Daha sonra Anasayfa harici diğer tüm sayfaları kapatınız.
+ */
+
+public class _02_WindowsSoru {
+}
